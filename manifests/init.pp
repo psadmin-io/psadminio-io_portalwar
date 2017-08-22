1309d4b3fad8e7261b9b1b9ce648839011d64d5e
Eric Bolinger <bolinger@esb.ad.cu.edu>
2017-08-22 11:19:51 -0600

    add init.pp and begin to populate

---
+++ manifests/init.pp
@@ -0,0 +1,10 @@
+class io_portalwar (
+) inherits io_portalwar::params {
+
+  #contain ::io_portalwar::configprop
+  #contain ::io_portalwar::psserver_shuf
+
+  if ($io_portalwar::params::rename_pia_cookie) {
+    contain ::io_portalwar::cookie_name
+  }
+}
