6abe75875456ea358bd1afe61d6855e364e0aac3
Eric Bolinger <bolinger@esb.ad.cu.edu>
2017-08-22 11:10:07 -0600

    Case for OS added to params.pp

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,3 +1,21 @@
 class io_portalwar::params (
+  $pia_domain_list = undef,
+  $pia_cookie_name = undef,
+  $configprop      = undef,
+  $psserver_list   = undef,
 ){
+
+  case $::facts['os']['name'] {
+    'AIX':     {
+    }
+    'Solaris': {
+    }
+    'windows': {
+    }
+    default:   {
+    }
+  }
+
+  validate_hash($pia_domain_list)
+
 }
